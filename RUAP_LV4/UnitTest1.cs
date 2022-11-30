86972fe3d3631f1bacaedfda53d5bdbe627a3cc4
EldinMesic <heallios88@gmail.com>
2022-11-30 17:41:11 +0100

    unittest1

--- RUAP_LV4/UnitTest1.cs
+++ RUAP_LV4/UnitTest1.cs
@@ -189,6 +189,96 @@ namespace SeleniumTests
 
 
 
+    public class Buy
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
 
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheBuyTest()
+        {
+            driver.Navigate().GoToUrl("https://demowebshop.tricentis.com/");
+            driver.FindElement(By.XPath("//div[3]/div/div/a/img")).Click();
+            driver.FindElement(By.Id("add-to-cart-button-31")).Click();
+            driver.FindElement(By.XPath("//li[@id='topcartlink']/a/span")).Click();
+            driver.FindElement(By.Id("checkout")).Click();
+            driver.FindElement(By.Id("termsofservice")).Click();
+            driver.FindElement(By.XPath("//button/span")).Click();
+            driver.FindElement(By.Id("checkout")).Click();
+            driver.FindElement(By.XPath("//input[@value='Checkout as Guest']")).Click();
+   
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
 
 }
